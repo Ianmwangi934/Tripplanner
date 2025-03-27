8ffa23fed27fc420e29af200d74a1ae7639f4dc5
ian mwangi <ian_mwangi@localhost.localdomain>
2025-03-27 13:03:53 +0300

    latest commit

--- my-react-app/src/components/Login.js
+++ my-react-app/src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://tripplanner-2.onrender.com/api/login/", {
+    const response = await fetch("https://tripplanner-2.onrender.com:10000/api/login/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
