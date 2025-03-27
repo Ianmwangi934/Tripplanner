54ecf2a8cd77d716b848f7d7de3a4f719987c160
ian mwangi <ian_mwangi@localhost.localdomain>
2025-03-27 11:12:15 +0300

    Debugging

--- my-react-app/src/components/Register.js
+++ my-react-app/src/components/Register.js
@@ -27,7 +27,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://127.0.0.1:8000/api/register/", {
+    const response = await fetch("https://tripplanner-2.onrender.com/api/register/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
