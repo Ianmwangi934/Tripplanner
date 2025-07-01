22493657fc3416040bc976befff130a41839294a
ian mwangi <ian_mwangi@localhost.localdomain>
2025-07-01 10:46:24 +0300

    New deployment

--- my-react-app/src/components/Login.js
+++ my-react-app/src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://tripplanner-2.onrender.com/api/login/", {
+    const response = await fetch("http://127.0.0.1:8000/api/login/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
