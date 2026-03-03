1f03b279456f35b20e8a8df6109d7b63b43051b0
Ianmwangi934 <ianmurigu4@gmail.com>
2026-03-03 06:57:57 +0300

    changed url for production

--- my-react-app/src/components/TripForm.js
+++ my-react-app/src/components/TripForm.js
@@ -44,7 +44,7 @@ const TripForm = () => {
         setLoading(true);
 
         try {
-            const response = await axios.post("http://127.0.0.1:8000/api/route/", formData);
+            const response = await axios.post("https://tripplanner-3.onrender.com/api/route/", formData);
             console.log("API Response:", response.data);
 
             if (response.data.geometry && response.data.geometry.coordinates) {
@@ -141,7 +141,7 @@ const TripForm = () => {
             }
         });
     };
-}, [logSheets, selectedActivity]);
+}, [logSheets, selectedActivity]);s
 
     return (
         
