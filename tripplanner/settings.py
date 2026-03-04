d457e1bf813c6857378d90eb06550953a917f383
Ianmwangi934 <ianmurigu4@gmail.com>
2026-03-04 03:28:47 +0000

    Update settings.py

--- tripplanner/settings.py
+++ tripplanner/settings.py
@@ -137,7 +137,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",# React development server
-    https://tripplanner-bljf.vercel.app,
+    "https://tripplanner-bljf.vercel.app",
     
     
 ]
